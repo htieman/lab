f43d82d94557a49d7ac8f698836a661c797194dc
htieman <118577949+htieman@users.noreply.github.com>
2022-11-20 19:25:39 -0600

    Update account.py

--- account.py
+++ account.py
@@ -1,13 +1,13 @@
-class Account:
-    def __init__(self, name):
+lass Account:
+    def __init__(self, name: str, bal=0) -> None:
         """
         Constructor used to create a new instance of the account class
         :param name: Name of account
         """
         self._account_name = name
-        self._account_balance = 0
+        self._account_balance = bal
 
-    def deposit(self, amount):
+    def deposit(self, amount: int) -> None:
         """
         deposits a given amount into an account
         :param amount: amount of money to be deposited
@@ -19,7 +19,7 @@ class Account:
             self._account_balance += amount
             return True
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: int) -> None:
         """
         withdraws a given amount from an account
         :param amount: amount of money to be withdrawn
@@ -31,14 +31,14 @@ class Account:
             self._account_balance -= amount
             return True
 
-    def get_balance(self):
+    def get_balance(self) -> int:
         """
         returns account balance
         :return: account balance
         """
         return self._account_balance
 
-    def get_name(self):
+    def get_name(self) -> str:
         """
         returns account name
         :return: account name
